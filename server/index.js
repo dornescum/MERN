9d1cc8e933547db67731cf4a001cbb731f1e6c87
Mihai Dornescu <dornescu@gmail.com>
2021-09-06 10:37:19 +0300

    routes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app =express();
 // limitare poze
 app.use(bodyParser.json({limit:"30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true }));
 
 app.use(cors());
+app.use('/posts', postRoutes);
+
 
 const CONNECTION_URL ='mongodb+srv://mernUser:ceapa_2000@thenetninja.ftnae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
