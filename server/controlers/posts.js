77411ed86a51506f3a998c1ea05b7f925fc30166
Mihai Dornescu <dornescu@gmail.com>
2021-09-06 10:52:20 +0300

    logic for get '/'

--- server/controlers/posts.js
+++ server/controlers/posts.js
@@ -1,5 +1,17 @@
 // toata logica pentru rute este aici
+import PostMessage from "../models/postMessage.js";
 
-export const getPosts =(req, res)=>{
-	res.send('this worklls')
+export const getPosts =async (req, res)=>{
+			// este async(dureaza pana gasesti in baza de date info)
+	try{
+		 const postMessage = await PostMessage.find();
+		 // si un raspuns
+		 res.status(200).json(postMessage);
+	}
+	catch(err){
+		res.status(404).json({message: err.message})
+	}
+}
+export const createPost =(req, res)=>{
+	res.send('post creation' )
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {getPosts} from "../controlers/posts.js";
+import {getPosts, createPost} from "../controlers/posts.js";
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
