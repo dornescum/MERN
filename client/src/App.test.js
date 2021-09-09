6a9181f7243716aa2e5e203eadb102ca015fabc6
Mihai Dornescu <dornescu@gmail.com>
2021-09-09 17:40:44 +0300

    test react withRedux

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,35 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, cleanup} from "@testing-library/react";
 import App from "./App";
+import {createStore} from "redux";
+import {Provider, useDispatch} from "react-redux";
+
+afterEach(cleanup);
+const startingState = [];
+function reducer(state =startingState, action){
+	switch (action.type){
+		case 'FETCH_ALL':
+			return {...state. post}
+		default:
+			return state
+	}
+}
+
+function renderWithRedux (component, {
+	initialState, store = createStore(reducer, initialState)} ={} )
+{
+	return {
+		...render(<Provider store={store}>{component}< /Provider>)
+	}
+}
 
 test('Memories', ()=>{
-	render(<App />);
+	renderWithRedux(<App />);
 	const searchTEST = screen.getByText('Memories');
 	expect(searchTEST).toBeInTheDocument();
 })
+
+// test('Memories', ()=>{
+// 	render(<App />);
+// 	const searchTEST = screen.getByText('Memories');
+// 	expect(searchTEST).toBeInTheDocument();
+// })
